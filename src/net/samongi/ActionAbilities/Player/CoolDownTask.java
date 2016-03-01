9fa63cda89105dbe992ed4029c796c1fbd6e3636
Alex Sammons <alsamon@yahoo.com>
2016-03-01 18:52:18 -0500

    Why did this not commit?

--- src/net/samongi/ActionAbilities/Player/CoolDownTask.java
+++ src/net/samongi/ActionAbilities/Player/CoolDownTask.java
@@ -6,25 +6,40 @@ import net.samongi.ActionAbilities.ActionAbilities;
 
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class CoolDownTask extends BukkitRunnable
+public class CooldownTask extends BukkitRunnable
 {
+	private static final int UPDATE_TICKS = 4;
+	
 	private static int milliToTick(long time){return (int) Math.floor(20 * time / 1000);}
 	
 	private final UUID player;
 	private final int slot;
 	private final int ticks;
 	
+	private final CooldownUpdateTask update_task;
+	
 	private int started_time = -1;
 	
-	public CoolDownTask(UUID player, int slot, int ticks)
+	public CooldownTask(UUID player, int slot, int ticks)
 	{
 		this.player = player;
 		this.slot = slot;
 		this.ticks = ticks;
+		
+		// Creating the task that will update the task bar.
+		this.update_task = new CooldownUpdateTask(CooldownTask.UPDATE_TICKS, this);
 	}
 	
+	/**Gets the player this cooldown is for
+	 * 
+	 * @return
+	 */
 	public UUID getPlayer(){return this.player;}
 	
+	/**Gets the slot this cooldown is for
+	 * 
+	 * @return
+	 */
 	public int getSlot(){return this.slot;}
 	
 	/**Activates the cooldown
@@ -32,8 +47,19 @@ public class CoolDownTask extends BukkitRunnable
 	 */
 	public void activate()
 	{
-		this.started_time = CoolDownTask.milliToTick(System.currentTimeMillis());
+		this.started_time = CooldownTask.milliToTick(System.currentTimeMillis());
+		this.update_task.activate();
 		this.runTaskLater(ActionAbilities.instance(), this.ticks);
+	}
+	
+	/**Checks to see if this task is the front cooldown
+	 * 
+	 * @return
+	 */
+	public boolean isFrontCooldown()
+	{
+		PlayerData data = ActionAbilities.instance().getPlayerManager().getPlayer(this.player);
+		return data.isFrontCooldown(this.slot, this);
 	}
 	
 	/**Gets the elapsed ticks of this cooldown
@@ -42,10 +68,23 @@ public class CoolDownTask extends BukkitRunnable
 	 */
 	public int getElapsedTicks()
 	{
-		int current_tick = CoolDownTask.milliToTick(System.currentTimeMillis());
+		int current_tick = CooldownTask.milliToTick(System.currentTimeMillis());
 		if(started_time < 0) return -1;
 		return current_tick - this.started_time;
 	}
+	/**Gets the total ticks that this cooldown will countdown for.
+	 * 
+	 * @return The total ticks
+	 */
+	public int getTotalTicks(){return this.ticks;}
+	
+	/**Will return the remaining number of ticks
+	 * However this will return negative if the cooldown has finished
+	 * BE WARY WARRIOR
+	 * 
+	 * @return
+	 */
+	public int getRemainingTicks(){return this.getTotalTicks() - this.getElapsedTicks();}
 	
 	
 	@Override
